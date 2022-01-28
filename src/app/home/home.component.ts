68e5910e27c906bff76932ab1f8bfe4c92cb2582
Thomas Meyer <thomasmmeyer88@gmail.com>
2022-01-27 20:43:05 -0600

    carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ApplicationRef, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  slideIndex = 1;
+
+  constructor(
+    private apr: ApplicationRef,
+    private cdr: ChangeDetectorRef
+  ) {
+  }
+
   alertHTML = `<a href="http://coslc.org/wp-content/uploads/2021/06/Christ-Our-Savior-Administrative-Assistant.pdf">Administrative Assistant</a>`;
   
   alertText = `Christ Our Savior is hiring a part time ${this.alertHTML}. To apply, please send your resume to <b>jobs@coslc.org</b>`
   
   watchText = `We invite you to join us online (Sunday at 10:30 AM) by watching our live stream or listening to our live broadcast.`
+  
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.showSlides(this.slideIndex);
+  }
+
+
+  plusSlides(n) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  currentSlide(n) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {this.slideIndex = 1}    
+    if (n < 1) {this.slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].setAttribute("display", "none");  
+    }
+    slides[this.slideIndex-1].setAttribute("display", "inline-block");  
+    this.cdr.detectChanges();
   }
 
 }
