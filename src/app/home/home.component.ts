706dc6462f1b02fad7b44d85efd4128a8e01eb26
Thomas Meyer <thomasmmeyer88@gmail.com>
2022-01-27 18:55:33 -0600

    alert

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-
+  alertHTML = `<a href="http://coslc.org/wp-content/uploads/2021/06/Christ-Our-Savior-Administrative-Assistant.pdf">Administrative Assistant</a>`;
+  alertText = `Christ Our Savior is hiring a part time ${this.alertHTML}. To apply, please send your resume to <b>jobs@coslc.org</b>`
   ngOnInit(): void {
   }
 
