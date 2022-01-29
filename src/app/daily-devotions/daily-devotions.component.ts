71fd51aa90a1d742a932725f2470bfb0eddc63cc
Thomas Meyer <thomasmmeyer88@gmail.com>
2022-01-29 16:21:20 -0600

    daily devotions

--- src/app/daily-devotions/daily-devotions.component.ts
+++ src/app/daily-devotions/daily-devotions.component.ts
@@ -8,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class DailyDevotionsComponent implements OnInit {
 
   constructor() { }
+  
+  dailyDevHeader = `Daily Devotions`;
 
+  dailyDevText = `We invite you to listen to Daily Devotions given by Lutheran Hour Ministries by visiting the link below.`
+  
+  dailyDevUrl = `https://www.lhm.org/dailydevotions/default.asp?date=20220129`;
+
+  dailyDevUrlText = `Daily Devotions from Lutheran Hour Ministries`;
   ngOnInit(): void {
   }
 
