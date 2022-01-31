584305ba0ce919f57591988721af57449bf0dd16
Thomas Meyer <thomasmmeyer88@gmail.com>
2022-01-31 16:16:33 -0600

    donate page sans images

--- src/app/donate/donate.component.ts
+++ src/app/donate/donate.component.ts
@@ -9,6 +9,16 @@ export class DonateComponent implements OnInit {
 
   constructor() { }
 
+  verse = `“Everything comes from you, and we give you only
+  what has come from your hands.”`;
+  verseSource = `1 Chronicles 29:14`;
+  giveText = `Here at Christ Our Savior, we believe that generous giving is one of the best ways to grow in our trust in God. For over 20 years, God has always provided the necessary resources to carry out our congregation’s mission and ministry. God has also generously provided for the people who generously contribute. As Christians, we recognize that God blesses us with time, talent and treasures. All <i>we have</i> actually are not ours but are gifts from God. He blesses us so we can serve Him and our neighbor. We thank you for your generous and faithful support to our ministry.  `
+  givePhysicalHeader = `By Mail or Bill Pay`;
+  churchName = `Christ Our Savior Lutheran Church`;
+  adrLn1 = `5323 Blanco Road`;
+  adrLn2 = `San Antonio, TX 78216`;
+  onlineOpt1 = `Online via Faithstreet`;
+  onlineOpt2 = `Online via Paypal`
   ngOnInit(): void {
   }
 
