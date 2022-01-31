aedce803fe4cc2570698495f7734bbe74dfb4ca2
Thomas Meyer <thomasmmeyer88@gmail.com>
2022-01-31 15:57:40 -0600

    join us component

--- src/app/join-us/join-us.component.ts
+++ src/app/join-us/join-us.component.ts
@@ -9,6 +9,17 @@ export class JoinUsComponent implements OnInit {
 
   constructor() { }
 
+  joinUsHeader = `Join Us`;
+  joinUsText = `At Christ Our Savior we have a passion for living, sharing, and teaching the love of Christ with all people. We invite everyone in the San Antonio and surrounding areas to come and worship with us. We invite you to join us in person at our church sanctuary every Sunday at 10:30 AM.`;
+  serviceHoursHeader = `Service Hours`;
+  directionsHeader = `Directions`;
+  dirChurchName = `Christ Our Savior Lutheran Church`;
+  dirAdLn1 = `5323 Blanco Road`;
+  dirAdLn2 = `San Antonio, TX 78216`;
+  phone = ` 210-732-7223`;
+  fax = ` 210-732-9288`;
+  email = ` admin@coslc.org`;
+  web = ` coslc.org`;
   ngOnInit(): void {
   }
 
